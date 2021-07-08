016b63a63aa53cbd084f8e497653dc5b34cef6ed
Brandon Cimino <bcimino@cccis.com>
2021-07-08 10:19:39 -0500

    Fixed annotations in LemmaAttribute.cs

--- Conjugal/Annotations/LemmaAttribute.cs
+++ Conjugal/Annotations/LemmaAttribute.cs
@@ -4,23 +4,27 @@ using Humanizer;
 
 using JetBrains.Annotations;
 
-namespace Conjugal.Annotations
-{
-    public class LemmaAttribute : Attribute
-    {
-        [NotNull]
-        public readonly string Lemma;
+namespace Conjugal.Annotations {
+    public class LemmaAttribute : Attribute {
+        [NotNull] public readonly string Lemma;
 
-        [NotNull] public readonly LetterCasing? Casing;
+        [CanBeNull] public readonly LetterCasing? Casing;
 
-        public LemmaAttribute(string lemma)
-        {
+        public LemmaAttribute([NotNull] string lemma) {
             Lemma = lemma;
-            Casing = null;
         }
 
-        public LemmaAttribute(string lemma, LetterCasing casing ) : this(lemma)
-        {
+        /// <summary>
+        /// Defines a <see cref="LemmaAttribute"/> for this class with the given <see cref="LetterCasing"/>.
+        ///
+        /// To use the default <see cref="LetterCasing"/>, use the alternate constructor, <see cref="LemmaAttribute(string)"/>.
+        /// </summary>
+        /// <remarks>
+        /// This constructor cannot be combined with <see cref="LemmaAttribute(string)"/> because <see cref="Nullable{T}"/>s cannot be used as attribute parameters.
+        /// </remarks>
+        /// <param name="lemma"></param>
+        /// <param name="casing"></param>
+        public LemmaAttribute([NotNull] string lemma, LetterCasing casing) : this(lemma) {
             Casing = casing;
         }
     }
