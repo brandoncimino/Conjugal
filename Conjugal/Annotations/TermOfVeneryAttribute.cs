99210029d9a4c2afd45523677b9fd1beba7409c3
brandoncimino <brandon.cimino@gmail.com>
2021-12-07 22:05:39 -0600

    Added some `<inheritdoc/>`s so I would stop getting yelled at

--- Conjugal/Annotations/TermOfVeneryAttribute.cs
+++ Conjugal/Annotations/TermOfVeneryAttribute.cs
@@ -1,15 +1,23 @@
 using JetBrains.Annotations;
 
 namespace FowlFever.Conjugal.Annotations {
+    /// <inheritdoc/>
     /// <summary>
-    /// The onanistic alias of <see cref="CollectiveNounAttribute"/>.
+    /// The onanistic alias of <see cref="T:FowlFever.Conjugal.Annotations.CollectiveNounAttribute"/>.
     /// </summary>
-    /// <seealso cref="CollectiveNounAttribute"/>
+    /// <seealso cref="T:FowlFever.Conjugal.Annotations.CollectiveNounAttribute"/>
     [PublicAPI]
     public class TermOfVeneryAttribute : CollectiveNounAttribute {
+        /// <inheritdoc/>
         public TermOfVeneryAttribute(string singular, Countability countability) : base(singular, countability) { }
+
+        /// <inheritdoc/>
         public TermOfVeneryAttribute(string singularAndPlural) : base(singularAndPlural) { }
+
+        /// <inheritdoc/>
         public TermOfVeneryAttribute(string singular, string plural) : base(singular, plural) { }
+
+        /// <inheritdoc/>
         public TermOfVeneryAttribute(string singular, string plural, Countability countability) : base(singular, plural, countability) { }
     }
 }
