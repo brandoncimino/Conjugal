53196600bab3108f82dc26f732d1af73d5592164
brandoncimino <brandon.cimino@gmail.com>
2021-12-07 23:59:36 -0600

    Explicitly called Humanizer's `ApplyCase`

--- Conjugal/LetterCasingExtensions.cs
+++ Conjugal/LetterCasingExtensions.cs
@@ -10,7 +10,7 @@ namespace FowlFever.Conjugal {
         /// <param name="str">the <see cref="string"/> to which we will <see cref="CasingExtensions.ApplyCase"/></param>
         /// <returns>a new <see cref="string"/> in this <see cref="LetterCasing"/></returns>
         public static string ApplyTo(this LetterCasing casing, string str) {
-            return str.ApplyCase(casing);
+            return CasingExtensions.ApplyCase(str, casing);
         }
     }
 }
