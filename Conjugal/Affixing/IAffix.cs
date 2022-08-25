4b5586b0abf4b82570ef4a75af815ab067f6b167
Brandon Cimino <bcimino@cccis.com>
2022-08-25 00:26:52 -0500

    added a fancy generic `IAffix{T}` and a bunch of flavored interfaces

--- Conjugal/Affixing/IAffix.cs
+++ Conjugal/Affixing/IAffix.cs
@@ -1,3 +1,5 @@
+using System;
+
 using JetBrains.Annotations;
 
 namespace FowlFever.Conjugal.Affixing {
@@ -15,10 +17,12 @@ namespace FowlFever.Conjugal.Affixing {
         /// The kind of <a href="https://en.wikipedia.org/wiki/affix">affix</a> being utilized.
         /// </summary>
         public AffixFlavor AffixFlavor { get; }
+
         /// <summary>
         /// A <see cref="string"/> interposed betwixt the stem and <see cref="BoundMorpheme"/>.
         /// </summary>
         public string? Joiner { get; }
+
         /// <summary>
         /// The <a href="https://en.wiktionary.org/wiki/bound_morpheme">bound morpheme</a> used to <see cref="AffixedExtensions.Render"/> the final result.
         /// </summary>
@@ -26,5 +30,50 @@ namespace FowlFever.Conjugal.Affixing {
         /// <i>-ing</i> in <i>genuflecting</i>
         /// </example>
         public string? BoundMorpheme { get; }
+
+        /// <summary>
+        /// A second <a href="https://en.wiktionary.org/wiki/bound_morpheme">bound morpheme</a>, used in addition to <see cref="BoundMorpheme"/> for certain <see cref="Affixing.AffixFlavor"/>s, like <see cref="Affixing.AffixFlavor.Circumfix"/>.
+        /// </summary>
+        /// <example>
+        /// (<see cref="BoundMorpheme"/>, <see cref="BoundMorpheme2"/>) = (<a href="https://en.wiktionary.org/wiki/em-_-en#English">em-, -en</a>)
+        /// </example>
+        public string? BoundMorpheme2 => default;
+
+        /// <summary>
+        /// Where in the <a href="https://en.wikipedia.org/wiki/Word_stem">linguistic stem</a> the <see cref="BoundMorpheme"/> will be placed.
+        /// </summary>
+        public Index InsertionPoint => default;
     }
+
+    /// <summary>
+    /// Represents a strongly typed <see cref="IAffix"/>, where the <typeparamref name="TFlavor"/> type parameter determines the <see cref="AffixFlavor"/>.
+    /// </summary>
+    /// <typeparam name="TFlavor">a subtype of <see cref="IAffix{TFlavor}"/> corresponding to one of the <see cref="Affixing.AffixFlavor"/>s</typeparam>
+    public interface IAffix<TFlavor> : IAffix where TFlavor : IAffix<TFlavor> {
+        AffixFlavor IAffix.AffixFlavor => AffixFlavorExtensions.FromInterface<TFlavor>();
+    }
+
+    /// <inheritdoc />
+    public interface ISuffix : IAffix<ISuffix> { }
+
+    /// <inheritdoc />
+    public interface IPrefix : IAffix<IPrefix> { }
+
+    /// <inheritdoc />
+    public interface ICircumfix : IAffix<ICircumfix> { }
+
+    /// <inheritdoc />
+    public interface IDisfix : IAffix<IDisfix> { }
+
+    /// <inheritdoc />
+    public interface IInfix : IAffix<IInfix> { }
+
+    /// <inheritdoc />
+    public interface IAmbifix : IAffix<IAmbifix> { }
+
+    /// <inheritdoc />
+    public interface ITransfix : IAffix<ITransfix> { }
+
+    /// <inheritdoc />
+    public interface IDuplifix : IAffix<IDuplifix> { }
 }
